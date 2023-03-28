66cb03cfa0334b86dd896f12dad78fef9b1d6b15
Krish Gupta <krishguptaa13@gmail.com>
2023-03-28 20:18:37 +0530

    change

--- src/js/About.js
+++ src/js/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className='aboutbody' id='about'>
         <div className="left">
-            <h1>Hii There!! kaise ho</h1>
+            <h1>Hii There!!</h1>
             <h2>
             This is Krish Gupta</h2>
             <button className='btn'><a href="https://docs.google.com/document/d/1CNLl_UyUQ1htoozDjhSPkB5oxBK1MbMK/edit?usp=drivesdk&ouid=112476948555225174066&rtpof=true&sd=true">RESUME</a></button>
