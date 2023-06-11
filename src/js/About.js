f5a278919e8dfa559447b0a8e5c25fb91ff54810
Krish Gupta <krishguptaa13@gmail.com>
2023-06-11 13:33:13 +0530

    comm

--- src/js/About.js
+++ src/js/About.js
@@ -1,23 +1,6 @@
 import React from 'react'
 import img1 from '../Images/bg2.webp'
 
-window.addEventListener('scroll', reveal);
-function reveal(){
-    var reveals = document.querySelectorAll('.reveal');
-    for(var i=0; i<reveals.length; i++){
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 150;
-
-        if(revealtop<windowheight-revealpoint){
-            reveals[i].classList.add('active');
-        }
-        else{
-            reveals[i].classList.remove('active');
-        }
-    }
-}
-
 
 export default function About() {
   return (
@@ -25,7 +8,7 @@ export default function About() {
     {/*}<img src={img1} alt=""/>{*/}
         <div className="left">
             <h1>Hii There!!</h1>
-            <h2>This is Krish Gupta</h2>
+            <h2>This is&nbsp; <span>K</span>rish&nbsp; <span>G</span>upta</h2>
             <button id='btna' className='abtn'><a href="">RESUME</a></button>
         </div>
     </div>
--- src/js/Aboutme.js
+++ src/js/Aboutme.js
@@ -12,9 +12,9 @@ export default function About_me() {
           <img src={img1} alt="" />
           </div>
           <div className="right">
-              <p>Hii there, this is Krish Gupta from Bhilai, Chhattisgarh. Currently I am pursuing my Bachelors in Mining Engeneering from, one of the reputed college of our country, that is National Institute of Technology. </p>
+              <p>Hii there, this is Krish Gupta from Bhilai, Chhattisgarh. Currently I am pursuing my Bachelors in Mining Engeneering from, one of the reputed college of our country, that is National Institute of Technology Raipur. 
 
-              <p>
+              
               Though I am pursuing my degree in mining, I am very much fascinated towards tech domain and coding is kind of pleasure to me. I started coding in C++ from 1st sem itself and futher in the 2nd sem I started learning WebDevelopment. Through practing I got a good command in frontend and Data Structures and Algorithms and currently I am working on my Backend Development aswell to enhance and add on new skills.
               </p>
           </div>
