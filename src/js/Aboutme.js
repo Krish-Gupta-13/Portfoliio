1855af1524c1b965a16813d6845c7d0a10bbb537
Krish Gupta <krishguptaa13@gmail.com>
2023-09-07 20:35:08 +0530

    updat

--- src/js/Aboutme.js
+++ src/js/Aboutme.js
@@ -12,10 +12,10 @@ export default function About_me() {
           <img src={img1} alt="" />
           </div>
           <div className="right">
-              <p>Hii there, this is Krish Gupta from Bhilai, Chhattisgarh. Currently I am pursuing my Bachelors in Mining Engeneering from, one of the reputed college of our country, that is National Institute of Technology Raipur. 
-
-              
-              Though I am pursuing my degree in mining, I am very much fascinated towards tech domain and coding is kind of pleasure to me. I started coding in C++ from 1st sem itself and futher in the 2nd sem I started learning WebDevelopment. Through practing I got a good command in frontend and Data Structures and Algorithms and currently I am working on my Backend Development aswell to enhance and add on new skills.
+              <p>Hii there, this is Krish Gupta from Bhilai, Chhattisgarh. Currently I am pursuing my Bachelors in Mining Engineering from, one of the reputed college of our country, that is National Institute of Technology, Raipur. 
+<br></br><br></br>
+          
+              Though I am pursuing my degree in mining, I am very much fascinated towards tech domain and coding is kind of pleasure to me. I have a good command on data structures and alogorithm and have solved over a lot of DSA problems. I am also interested towards the development side and have gained the knowledge of HTML, CSS, JavaScript, ReactJs, Bootstrap as a knowledge of front end development. I am a beginner in backend development and enhancing my skills to become a full-stack deleloper. 
               </p>
           </div>
       </div>
